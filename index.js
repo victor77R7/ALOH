059e2cc84408e0b9f1670634c20f46a2ea9b15a5
victor salcedo <salcedov909@gmail.com>
2024-08-17 05:07:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,1 @@
+const Parse = require('parse/node');\n\nParse.initialize("YOUR_APP_ID", "YOUR_JAVASCRIPT_KEY");\nParse.serverURL = 'https://YOUR_PARSE_SERVER:1337/parse';\n\nconsole.log('Parse initialized!');
